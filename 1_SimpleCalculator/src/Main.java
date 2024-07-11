3de8d99211b0b0deba1b925d6b887a513f1b82a2
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-07-11 22:56:59 +0800

    feat: Calculator interface

---
+++ 1_SimpleCalculator/src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+/**
+ * @author ZZHow
+ * @date 2024/7/11
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("SimpleCalculator 1.0.0");
+        Pane load = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("SimpleCalculator.fxml")));
+        Scene scene = new Scene(load);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
